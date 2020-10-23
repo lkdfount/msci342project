6caa17f793e017d3265a56c98f4bee909d97ddfb
emilynairnn <72414927+emilynairnn@users.noreply.github.com>
2020-10-23 08:41:34 -0400

    Updated front end of Home Page
    MVP for home page

--- travvy/src/components/Home.vue
+++ travvy/src/components/Home.vue
@@ -1,34 +1,156 @@
 <template>
   <div class="Home">
     <h1>{{ msg }}</h1>
-    <h1>Home page</h1>
+    <h1 class="travvy" style="float:left">TRAVVY Home Page</h1><br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+        <center><img src="src/images/TRAVVY_logo.png" alt="logo"/></center>
+        <br>
+        <input type="button" value="Profile" v-on:click="reverseMessage" style="float: right;" class="profile"> <br>
+        <label>{{ destination }}</label>
+        <label for="Destination">Select a Destination:</label>
+        <select id="Destination" name="Destination" class="destination">
+          <option value="Toronto">Toronto</option>
+          <option value="New York City">New York City</option>
+        </select>
+        <label>{{ dates }}</label>
+        <label for="Dates">     Select Departure and Return Dates:</label>
+          <input type="text" class="dates"> 
+        <label>{{ travellers }}</label>
+        <label for="Travellers">     Number of Travellers</label>
+          <input type="text" class="travellers">
+        <br>  
+        <br>       
+        
+        <center><button v-on:click="reverseMessage" class="search">Search</button></center>
+    
+        <p>{{ uploadDocs }}</p>
+        <p>Upload Travel Documents Below:</p>
+        <p>{{ uploadFiles}}</p>
+        <p>Drop files here or <button class="selectFiles">Select Files</button> </p>
+    
+        <p> {{ nextDestination }}</p>
+        <p>Find your next travel destination</p>
+        <button v-on:click="reverseMessage" class="nextDestination">Toronto</button>   <button v-on:click="reverseMessage" class="nextDestination">New York City</button>   <button v-on:click="reverseMessage" class="nextDestination">Rome</button>   <button v-on:click="reverseMessage" class="nextDestination"> Rio De Janeiro</button>
+   </div>
+  
+   
+ </template>
 
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'Home',
-  props: {
-    msg: String
+ <script>
+ export default {
+   name: 'Home',
+   props: {
+     msg: String,
+     travvy: String,
+     destination:String,
+     dates: String,
+     travellers: String,
+     uploadDocs: String,
+     uploadFiles: String,
+     nextDestination: String,
+     Toronto: String,
+     newYorkCity: String,
+     rome: String,
+     rioDeJaneiro: String
+   },
+   methods: {
+           reverseMessage: function () {
+             this.message = this.message.split('').reverse().join('')
+         }
+    
   }
 }
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+ </script>
+ 
+ <!-- Add "scoped" attribute to limit CSS to this component only -->
+ <style scoped>
+.travvy{
+  background-color: #B8D1FF;
+  border: none;
+  color: #FF5F00;
+  padding: 10px 10px;
+  text-align: left;
+  text-decoration: none;
   display: inline-block;
-  margin: 0 10px;
+  font-size: 40px;
+  border-radius: 12px;
 }
-a {
-  color: #42b983;
+.profile{
+  background-color: #FF5F00;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  border-radius: 12px;
 }
-</style>
+.destination{
+  background-color: #C7EEA9;
+  border-radius: 12px;
+}
+.dates{
+  background-color: #C7EEA9;
+  border-radius: 12px;
+}
+.travellers{
+  background-color: #C7EEA9;
+  border-radius: 12px;
+}
+.search{
+  background-color: #FF5F00;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  border-radius: 12px;
+   }
+.selectFiles{
+  background-color: #C7EEA9;
+  border: none;
+  color: black;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 12px;
+  border-radius: 12px;
+}
+.nextDestination{
+  background-color: #B8D1FF;
+  border: none;
+  color: black;
+  padding: 32px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  border-radius: 12px;
+}
+
+ h3 {
+   margin: 40px 0 0;
+ }
+ ul {
+   list-style-type: none;
+   padding: 0;
+ }
+ li {
+   display: inline-block;
+   margin: 0 10px;
+ }
+ a {
+   color: #42b983;
+ }
+ </style>
