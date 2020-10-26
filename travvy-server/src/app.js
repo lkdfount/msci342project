1cf3e34a24880b0845a0c8fa24687dce08634f8e
dyampols <daria.yampolsky@gmail.com>
2020-10-26 02:49:33 -0400

    changed file reference

--- travvy-server/src/app.js
+++ travvy-server/src/app.js
@@ -19,7 +19,7 @@ if(process.env.NODE_ENV == 'production'){
   app.use(express.static('../public'))
 
   //Handle single page 
-  app.get(/.*/,(req, res) => res.sendFile(__dirname + '/public/index.html'))
+  app.get(/.*/,(req, res) => res.sendFile('../public/index.html'))
 }
 
 // Setup sequelize server
