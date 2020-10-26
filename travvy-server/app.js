34761964f7740be558be464864af599d5f7f5b28
dyampols <daria.yampolsky@gmail.com>
2020-10-26 08:51:40 -0400

    changed routes location

--- travvy-server/app.js
+++ travvy-server/app.js
@@ -11,7 +11,7 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-require('./routes')(app)
+require('./routes/routes')(app)
 
 //Handle production 
 if(process.env.NODE_ENV == 'production'){
