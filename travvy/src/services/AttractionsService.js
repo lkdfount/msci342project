0bdf7c73d8672ca95baccba258c79689467ec347
dyampols <daria.yampolsky@gmail.com>
2020-11-05 21:14:36 -0500

    started adding changes

--- travvy/src/services/AttractionsService.js
+++ travvy/src/services/AttractionsService.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 
 export default {
   // It makes get request to recommend attractions based on city
-    recommend(city,groupSize) {
-        return axios.get('/recommend', {params:city,groupSize})  
+    recommend(city,groupSize,startDate,endDate) {
+        return axios.get('/recommend', {params:city,groupSize,startDate,endDate})  
           .catch(function (error) {
             console.log(error);
           });
