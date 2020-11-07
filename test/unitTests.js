d27c874ce5fc4417c4bcbbdba3d8bcfa15e342ea
dyampols <daria.yampolsky@gmail.com>
2020-11-07 10:13:33 -0500

    changed gitignore

--- test/unitTests.js
+++ test/unitTests.js
@@ -7,7 +7,7 @@ const startDate = new Date("2020-11-05")
 const endDate = new Date("2021-01-05")
 
 describe('Reccommend Attractions Test', function() {
- it('should return 4', async function() {
+ it('Reccomend Attractions should return 4', async function() {
        const response = await RecommendController.recommend(city,groupSize,startDate,endDate)
        assert.equal(response.length,4);
         })
