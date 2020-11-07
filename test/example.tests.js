70e631c4058bcbc9b7d9c19fba6c6fffa5f19c97
dyampols <daria.yampolsky@gmail.com>
2020-11-06 19:52:47 -0500

    added unit testing structure

---
+++ test/example.tests.js
@@ -0,0 +1,17 @@
+const RecommendController = require('../travvy-server/src/controllers/RecommendController.js')
+
+const assert = require('assert');
+const city = "Toronto"
+const groupSize = 20
+const startDate = new Date("2020-11-05")
+const endDate = new Date("2021-01-05")
+
+describe('Reccommend Attractions Test', function() {
+ it('should return 4', async function() {
+       const response = await RecommendController.recommend(city,groupSize,startDate,endDate)
+       console.log(response.length)
+       assert.equal(response.length,4);
+        })
+
+    });
+
